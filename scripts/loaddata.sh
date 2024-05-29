ab44508ec3ab2964a1bf86413f9b5ee0d0156f1b
Edison Peñuela <edisonpe961206@hotmail.com>
2024-05-29 18:19:45 -0500

    Add Initial Data

--- scripts/loaddata.sh
+++ scripts/loaddata.sh
@@ -3,6 +3,6 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 ./exec.sh python manage.py loaddata autores
-# ./exec.sh python manage.py loaddata generos
-# ./exec.sh python manage.py loaddata editoriales
-# ./exec.sh python manage.py loaddata libros
+./exec.sh python manage.py loaddata generos
+./exec.sh python manage.py loaddata editoriales
+./exec.sh python manage.py loaddata libros

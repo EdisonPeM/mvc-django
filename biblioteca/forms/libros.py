dc714712717f9a4924b09b1011f0bf5534eedca1
Edison Peñuela <edisonpe961206@hotmail.com>
2024-05-28 23:48:53 -0500

    update libros form and form template

--- biblioteca/forms/libros.py
+++ biblioteca/forms/libros.py
@@ -1,13 +1,43 @@
+from django import forms
+
 from .baseForm import BaseModelForm
 
 from ..models.libros import Libro
 
+labels = {
+  'codigo_libro': 'Código del Libro',
+  'nombre_libro': 'Nombre del Libro',
+  'existencias': 'Existencias',
+  'precio': 'Precio',
+  'descripcion': 'Descripción',
+  'codigo_autor': 'Autor',
+  'codigo_editorial': 'Editorial',
+  'id_genero': 'Genero'
+}
+
+precioField = forms.DecimalField(
+  required=True,
+  max_digits=10,
+  decimal_places=2,
+  min_value=0,
+  label=labels["precio"],
+  help_text = "En pesos colombianos",
+)
+# Additional symbol to show in the UI
+precioField.prefix = '$'
+
 class CrearLibrosForm(BaseModelForm):
+  precio = precioField
+  
   class Meta():
     model = Libro
     fields = "__all__"
+    labels = labels
     
 class EditarLibrosForm(BaseModelForm):
+  precio = precioField
+
   class Meta():
     model = Libro
     exclude = ["codigo_libro"]
+    labels = labels

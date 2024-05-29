8ae47fa93380eeab655722de689ab0417816b772
Edison Peñuela <edisonpe961206@hotmail.com>
2024-05-29 16:58:08 -0500

    Update models

--- biblioteca/models/libros.py
+++ biblioteca/models/libros.py
@@ -13,8 +13,8 @@ class Libro(models.Model):
   descripcion = models.TextField()
   # Foreign Keys
   autor = models.ForeignKey(Autor, on_delete=models.CASCADE, db_column='codigo_autor', related_name='libros')
-  editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE, db_column='codigo_editorial', related_name='libros')
   genero = models.ForeignKey(Genero, on_delete=models.CASCADE, db_column='id_genero', related_name='libros')
+  editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE, db_column='codigo_editorial', related_name='libros')
   # Biblioteca Data
   existencias = models.PositiveIntegerField()
   precio = models.DecimalField(max_digits=10, decimal_places=2)
